c0072a5a980975490a2600d8dba1f5474b3b1a13
V-keerththanan <keertht-se19040@stu.kln.ac.lk>
2022-04-21 13:22:44 +0530

    another chane

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass{
     public static void main(String args[]) {
-        System.out.println("Byee");
+        System.out.println("come back");
         Account x=new Account();
         x.show();
     }
