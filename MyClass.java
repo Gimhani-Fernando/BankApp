838fce99ef14cd44cebd5d7cbe1ccb923e2b0d2c
V-keerththanan <keertht-se19040@stu.kln.ac.lk>
2022-04-21 13:29:40 +0530

    new change

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass{
     public static void main(String args[]) {
-        System.out.println("come back");
+        System.out.println("new change");
         Account x=new Account();
         x.show();
     }
