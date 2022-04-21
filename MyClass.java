e1bb9ae09c9755f61abe46f1ea6b561397cfe4da
Gimhani Fernando <fernand-se19023@stu.kln.ac.lk>
2022-04-21 13:56:36 +0530

    I did it

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass{
     public static void main(String args[]) {
 
-        System.out.println(" I hate java wt**** relay hate ");
+        System.out.println(" I love java wt**** relay hate ");
 
         Account x=new Account();
         x.show();
