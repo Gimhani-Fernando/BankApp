5632eb41df67f1993658d7c58691f8b84e38745a
V-keerththanan <keertht-se19040@stu.kln.ac.lk>
2022-04-21 13:52:44 +0530

    againagain

--- MyClass.java
+++ MyClass.java
@@ -1,10 +1,8 @@
 public class MyClass{
     public static void main(String args[]) {
-<<<<<<< HEAD
-        System.out.println("new change bl bla");
-=======
-        System.out.println("change");
->>>>>>> cdd16a4dcc7d0d0f8273a376d05cee5702bf9735
+
+        System.out.println(" I hate java wt**** relay hate ");
+
         Account x=new Account();
         x.show();
     }
