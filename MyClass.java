ca5246a202c059a98e3121a875e7088493924f97
Gimhani Fernando <fernand-se19023@stu.kln.ac.lk>
2022-04-21 13:46:49 +0530

    finallyyyyyy

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass{
     public static void main(String args[]) {
-        System.out.println("new change bla bla");
+        System.out.println("new change bl bla");
         Account x=new Account();
         x.show();
     }
