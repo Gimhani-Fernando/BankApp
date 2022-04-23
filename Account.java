ac27c02bfdaae62985e9dc8ab48d9768f464fe1f
Gimhani Fernando <fernand-se19023@stu.kln.ac.lk>
2022-04-23 13:41:05 +0530

    scanner leaks

--- Account.java
+++ Account.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Account {
-    public void show(){
-        System.out.println("Hi account holder ");
+    private String name;
+    private String NIC;
+    private float initial_amount;
+    private int ac_num=0;
+
+    public void setData(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your full name");
+        this.name=sc.next();
+        System.out.println("Enter your NIC number");
+        this.NIC=sc.next();
+        boolean init_bool=true;
+
+        while(init_bool){
+            System.out.println("Enter the initial amount you wish to provide");
+            float init=sc.nextFloat(); 
+
+            if(init>=500){
+                System.out.println("Account is successfully created");
+                this.initial_amount=sc.nextFloat(); init_bool=false;
+
+            }else{
+                System.out.println("Please provide an initial amount more than or equal to 500");
+            }
+        }
+        sc.close();
+    }
+
+    public void getData(){
+        System.out.println("\nAccount number "+ac_num+" details");
+        System.out.println("Account holder details: ");
+        System.out.println("NIC: "+this.NIC+"\nName: "+this.name);
     }
 }
--- MyClass.java
+++ MyClass.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 public class MyClass{
     public static void main(String args[]) {
-
-        System.out.println(" I love java wt**** relay hate ");
+        System.out.println(" Hello! Welcome to Bank Managment App\n ");
+        showOptions();
+        Scanner sc=new Scanner(System.in);
+        int option = sc.nextInt();
 
         Account x=new Account();
-        x.show();
+
+        while(option!=0){
+            switch(option){
+                case 1:
+                    x.setData(); break;
+                case 2:
+                    x.getData();break;
+                default:
+                    System.out.println("Invalid option");
+            }
+            System.out.println("\n\nDo you wish to proceed? ");
+            showOptions();
+            option = sc.nextInt();
+        }
+
+        System.out.println("Thank you! Have a nice day...");
+    }
+
+    public static void showOptions(){
+        System.out.println("\nEnter 1 to create new account");
+        System.out.println("Enter 2 to view account holders record");
+        System.out.println("Enter 3 to withdraw and deposit amount");
+        System.out.println("Enter 4 to balance inquiry");
+        System.out.println("Enter 5 to close an account");
+        System.out.println("Enter 6 to edit account details");
+        System.out.println("Enter 0 to exit");
     }
 }
